7a5089fd35f3bdcca5de0d559d2b8c5f6a0029a8
Mami <nozaru.yeah@gmail.com>
2019-07-21 00:00:29 +0900

    add BitcoinAPI & TODO

---
+++ vue_todo/js/main.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: "#app",
+    data:{
+        newItem: '',
+      todos: []
+    },
+    methods: {
+        addItem: function(event){
+        //	alert();
+        if(this.newItem == '') return;
+        var todo = {
+          item: this.newItem,
+          isDone: false
+        }
+        this.todos.push(todo)
+      },
+      deleteItem: function(index){
+          //alert(index);
+        this.todos.splice(index,1)
+      }
+    }
+   })
